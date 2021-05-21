e57e09eeaa6a61d946f74fd460adef6610d1f3f2
Eduard <alehined@mail.ru>
2021-05-22 02:47:02 +0300

    add lesson-2

---
+++ lesson-2/src/index.js
@@ -0,0 +1,20 @@
+
+import React from "react"
+import ReactDOM from "react-dom"
+import { MessageField } from "./components"
+
+
+const App = () => {
+    return (
+        <div>
+            <MessageField />
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <>
+        <App />
+    </>,
+    document.getElementById("root"),
+)
