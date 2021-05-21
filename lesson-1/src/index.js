6e1344d071b9229d608e3d2d43c5cf34408d19d6
Eduard <alehined@mail.ru>
2021-05-22 02:33:31 +0300

    add lesson-2

--- lesson-1/src/index.js
+++ lesson-1/src/index.js
@@ -1,41 +1,20 @@
 
 import React from "react"
 import ReactDOM from "react-dom"
+import { MessageField } from "./components"
 
 
-const messages = ["send"]
-
-const sendMessage = () => {
-    messages.push("Нормально")
-
-    render()
-}
-
-const Message = ({ message }) => {
+const App = () => {
     return (
-        <React.Fragment>
-            <h1>{message}</h1>
-
-        </React.Fragment>
+        <div>
+            <MessageField />
+        </div>
     )
 }
 
-const MessageField = ({ messages }) => {
-    return (
-        <>
-            <button onClick={sendMessage}>send</button>
-            {messages.map((message, index) => (
-                <Message message={message} key={index} isVisible={true} />
-            ))}
-        </>
-    )
-}
-
-const render = () => {
-    ReactDOM.render(
-        <MessageField messages={messages} />,
-        document.getElementById("root"),
-    )
-}
-
-render()
+ReactDOM.render(
+    <>
+        <App />
+    </>,
+    document.getElementById("root"),
+)
